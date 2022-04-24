852ca3a27176f8c2db28a75415c911c44555f052
Folarinjr <folarinademola2@gmail.com>
2022-04-24 20:52:11 +0100

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+const App = () => {
+  const [password, setPassword] = useState('*******');
+
+  //Generate random number then change it to string, toString(36) this method includes alphabeth 
+  const randomPasskey = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+
+  //Set Password
+  const handlePassword = () => {
+    setPassword(randomPasskey);
+    toast('Password Generated')
+  }
+
+  const copy = async () => {
+    try {
+      await navigator.clipboard.writeText(randomPasskey);
+      toast('Copied')
+    } catch (error) {
+      toast('Not Copied')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='header'>Generate a password</h1>
+      <div className="content">
+        <h2 className='password'>{password}</h2>
+        <button className='btn' onClick={handlePassword}>Generate Password</button>
+        <button className="btn btn-primary" onClick={copy}>Copy</button>
+      </div>
+      <ToastContainer/>
     </div>
   );
 }
